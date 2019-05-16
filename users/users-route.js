ec6eca2f2a56d9aaa1c24c41d5d1734d17423ea5
Josh Akeman <joshakeman@gmail.com>
2019-05-16 19:06:46 -0400

    test

--- users/users-route.js
+++ users/users-route.js
@@ -4,10 +4,22 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/users', async (req, res) => {
-    const rows = await Users.getAll();
+router.get('/', async (req, res) => {
+    // const rows = await Users.getAll();
   
-    res.status(200).json(rows);
+    res.status(200).send()
+  });
+
+  router.post('/', async (req, res) => {
+    const user = await Users.insert(req.body);
+
+    res.status(200).json(user);
+  });
+
+  router.delete('/', async (req, res) => {
+    const user = await Users.remove(req.body.id);
+  
+    res.status(200).json(`User ${user} deleted`);
   });
 
   module.exports = router
